61d2d4f7894b02471a140673650f6f9855db401c
Toivo Sults <tosult@taltech.ee>
2025-09-04 22:53:19 +0300

    data annotation attributes

--- Domain.App/Isik.cs
+++ Domain.App/Isik.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Domain.Base;
 
 namespace Domain.App;
@@ -6,12 +7,15 @@ public class Isik : DomainEntityId
 {
     public Guid OsavotumaksId { get; set; }
     
+    [MaxLength(64)]
     public string Eesnimi { get; set; }
     
+    [MaxLength(64)]
     public string Perenimi { get; set; }
     
     public int Isikukood { get; set; }
     
+    [MaxLength(1500)]
     public string? Lisainfo { get; set; }
     
     public ICollection<IsikYritusel>? IsikudYritusel { get; set; }
--- Domain.App/JurIsik.cs
+++ Domain.App/JurIsik.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Domain.Base;
 
 namespace Domain.App;
@@ -8,10 +9,12 @@ public class JurIsik : DomainEntityId
     
     public Guid JurIsikLiikId { get; set; }
     
+    [MaxLength(128)]
     public string Nimi { get; set; }
     
     public int Registrikood { get; set; }
     
+    [MaxLength(5000)]
     public string? Lisainfo { get; set; }
     
     public ICollection<IsikYritusel>? IsikudYritusel { get; set; }
