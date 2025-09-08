a92c4313973a669ac96a6e0a41b2da4bf7cef453
Toivo Sults <tosult@taltech.ee>
2025-09-08 21:15:09 +0300

    Initial seeding

--- DAL.EF.App/Seeding/AppDataInit.cs
+++ DAL.EF.App/Seeding/AppDataInit.cs
@@ -1,6 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace DAL.EF.App.Seeding;
 
 public static class AppDataInit
 {
-    // TODO: Initial DB setup (Ürituse rollid, ettevõtlusvormid, tasumise viisid)
+    public static void MigrateDataBase(ApplicationDbContext context)
+    {
+        context.Database.Migrate();
+    }
+
+    public static void DropDataBase(ApplicationDbContext context)
+    {
+        context.Database.EnsureDeleted();
+    }
+
+    public static void SeedAppData(ApplicationDbContext context)
+    {
+        // TODO: SeedAppDataRollid();
+        // TODO: SeedAppDataVormid();
+        // TODO: SeedAppDataViisid();
+        context.SaveChanges();
+    }
 }
