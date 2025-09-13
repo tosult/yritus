507940e40476d27776df97300bc9409f07b5b894
Toivo Sults <tosult@taltech.ee>
2025-09-13 17:41:53 +0300

    Validate Yritus form

--- Domain.App/Yritus.cs
+++ Domain.App/Yritus.cs
@@ -3,17 +3,44 @@ using Domain.Base;
 
 namespace Domain.App;
 
-public class Yritus : DomainEntityId
+public class Yritus : DomainEntityId, IValidatableObject
 {
+    [Required]
     [MaxLength(128)]
     public string Nimi { get; set; }
     
+    [Required]
     public DateTime Algus { get; set; }
     
+    [Required]
     public DateTime Lopp { get; set; }
     
+    [Required]
     [MaxLength(128)]
     public string Asukoht { get; set; }
     
     public ICollection<IsikYritusel>?  IsikudYritusel { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Algus < DateTime.Now)
+        {
+            yield return new ValidationResult(
+                "Algus peab olema tulevikus!",
+            new[] {nameof(Algus)});
+        }
+        
+        if (Lopp < DateTime.Now)
+        {
+            yield return new ValidationResult(
+                "Lõpp peab olema tulevikus!",
+                new[] {nameof(Algus)});
+        }
+        if (Lopp <= Algus)
+        {
+            yield return new ValidationResult(
+                "Lõpp ei saa olla enne algust!",
+                new[] {nameof(Algus)});
+        }
+    }
 }
